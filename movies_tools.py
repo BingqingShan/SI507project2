e4dac6e53d188932a6726ceb52701c4eb8563a9c
BingqingShan <bqshan@umich.edu>
2019-02-18 22:42:08 -0500

    Diagram

--- movies_tools.py
+++ movies_tools.py
@@ -7,20 +7,12 @@ with open("movies_clean.csv", "r") as f:
 	total = []
 	for i in reader:
 		total.append(i)
-	list = []
-	for i in range(len(total)):
-		title = total[i][0]
-		rating = total[i][14]
-	# print(list)
+
 total_number=len(total)
 # total_number = len(total) # the total number of movies
-# print(total_number)
-# print(list[0])
-# list : the total of movie+rating as a list in a big list
+
 #title: single movie title
 #rating: single movie rating
-
-
 class Movie:
 	def __init__(self):
 		random_movie=random.choice(total)
